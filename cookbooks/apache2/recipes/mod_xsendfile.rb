099803e2eb58247d973d9a3a0af62044f3b6079e
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2012-02-12 20:30:01 +0100

    Add cookbooks apt, lograte, apache2, memcached, sudo

--- cookbooks/apache2/recipes/mod_xsendfile.rb
+++ cookbooks/apache2/recipes/mod_xsendfile.rb
@@ -1,27 +1,16 @@
-#
-# Cookbook Name:: apache2
-# Recipe:: mod_xsendfile 
-#
-# Copyright 2011, CustomInk, LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+require_recipe "apache2"
 
-case node['platform']
-when "debian","ubuntu"
-  package "libapache2-mod-xsendfile"
-when "centos","redhat","scientific","fedora"
-  package "mod_xsendfile"
+remote_directory "/usr/local/build/mod_xsendfile" do
+  source "mod_xsendfile"
+end
+
+execute "build and install mod_xsendfile" do
+  command "cd /usr/local/build/mod_xsendfile && apxs2 -ci mod_xsendfile.c"
+  not_if { File.exists?("/usr/lib/apache2/modules/mod_xsendfile.so") }
+end
+
+template "/etc/apache2/mods-available/xsendfile.load" do
+  source 'xsendfile.load.erb'
 end
 
 apache_module "xsendfile"
