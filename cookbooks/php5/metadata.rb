51ae66a0c73229a92e89b0158802a74206833fbf
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2012-02-12 20:59:13 +0100

    Update php.ini

--- cookbooks/php5/metadata.rb
+++ cookbooks/php5/metadata.rb
@@ -1,5 +1,3 @@
-maintainer        "37signals"
-maintainer_email  "sysadmins@37signals.com"
 description       "Configures php5"
-version           "0.1"
+version           "0.6"
 depends           "apache2"
--- cookbooks/php5/recipes/default.rb
+++ cookbooks/php5/recipes/default.rb
@@ -5,25 +5,13 @@ end
 
 package "libmcrypt4"
 package "libltdl7"
-package "apache2-mpm-worker"
-package "libapache2-mod-fcgid"
-apache_module "fcgid"
+package "apache2"
+package "libapache2-mod-php5"
+package "php5"
+package "php5-cli"
+package "php5-dev"
 
-bash "install_php" do
-  code <<-EOC
-wget #{node[:php5][:dist_url]}
-tar -C /#{node[:php5][:path].split("/")[1..-2].join("/")} -xpf #{node[:php5][:tar_pkg]}
-EOC
-  user "root"
-  cwd "/tmp"
-  not_if { File.directory?(node[:php5][:path]) }
-end
-
-link "/usr/bin/php-cgi" do
-  to "/usr/local/php/bin/php-cgi" 
-end
-
-template "/usr/local/php/lib/php.ini" do
+template "/etc/php5/apache2/php.ini" do
   source "php.ini.erb"
   owner "root"
   group "root"
