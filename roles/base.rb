9395f5911f4ffe97489d7ad2208ccc26a95909a2
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2012-02-12 20:11:02 +0100

    Add lamp role

--- roles/base.rb
+++ roles/base.rb
@@ -1,10 +1,10 @@
 name "base"
 description "Base server configuration"
 
-run_list {
-	"recipe[fail2ban]"
-	"recipe[ntp]"
-	"recipe[git]"
-	"recipe[zsh]"
+run_list(
+	"recipe[fail2ban]",
+	"recipe[ntp]",
+	"recipe[git]",
+	"recipe[zsh]",
 	"recipe[oh-my-zsh]"
-}
+)
---
+++ roles/lamp.rb
@@ -0,0 +1,8 @@
+name "lamp"
+description "LAMP server (apache,mysql,php)"
+
+run_list(
+	"recipe[apache2]",
+	"recipe[mysql::server]",
+	"recipe[php]"
+)
